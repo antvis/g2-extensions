ace464f46ca07c6e4e59a03dc4230888a2c7e871
wb-xcf804241 <wb-xcf804241@wb-xcf804241deMacBook-Pro.local>
2023-12-04 09:41:57 +0800

    feat(build): add rollup

---
+++ plot/rollup.config.mjs
@@ -0,0 +1,52 @@
+import commonjs from "@rollup/plugin-commonjs";
+import nodeResolve from "@rollup/plugin-node-resolve";
+import terser from "@rollup/plugin-terser";
+import typescript from "@rollup/plugin-typescript";
+import { readFileSync } from "fs";
+
+export default createConfig({
+  pkg: JSON.parse(readFileSync(new URL("./package.json", import.meta.url), "utf8")),
+  umdName: "G2.ExtensionPlot",
+  external: ["@antv/g", "@antv/g2"],
+  globals: {
+    "@antv/g": "window.G",
+    "@antv/g2": "window.G2",
+  },
+});
+
+function createConfig({ pkg, external = [], umdName = "", globals = {}, plugins = [] }) {
+  const sharedPlugins = [
+    ...plugins,
+    nodeResolve({
+      mainFields: ["module", "browser", "main"],
+      extensions: [".js", ".jsx", ".ts", ".tsx", ".es6", ".es", ".mjs"],
+    }),
+    commonjs(),
+    typescript({ sourceMap: true }),
+  ];
+
+  return [
+    {
+      input: "src/index.ts",
+      output: {
+        format: "umd",
+        file: pkg.unpkg,
+        name: umdName,
+        globals,
+        sourcemap: true,
+      },
+      external,
+      plugins: [
+        ...sharedPlugins,
+        terser({
+          compress: {
+            pure_getters: true,
+            unsafe: true,
+            unsafe_comps: true,
+            warnings: false,
+          },
+        }),
+      ],
+    },
+  ];
+}
